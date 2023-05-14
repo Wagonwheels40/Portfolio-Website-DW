c196e504bf58d4ef0fcdbb14b615c5ea607e5a07
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-14 12:38:18 +1000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('#contact-form');
+const button = form.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    sendData(); // Call the function to send the data to the server
+})
+
+function sendData() {
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const message = document.querySelector('#message');
+
+    if (name.trim === '') {
+        alert('Please enter your name.');
+        return;
+    }
+
+    if (email.trim === '') {
+        alert('Please enter your email address.');
+        return;
+    }
+
+    if (message.trim === '') {
+        alert('Please type your message.');
+        return;
+    }
+
+    // Sending the email using mailto URL
+    const mailtoURL = `mailto:wagonwheels40@tutanota.com?subject=Message from ${name}&body=${message}`;
+    window.location.href = mailtoUrl;
+
+    // Reset the form
+    form.reset();
+};
