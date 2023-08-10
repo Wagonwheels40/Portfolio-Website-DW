90d45ee4f30b5bd6c6faceaf017ac66c79ffad54
Daniel <116485787+Wagonwheels40@users.noreply.github.com>
2023-08-10 19:25:09 +1000

    Update language.js

--- language.js
+++ language.js
@@ -1,9 +1,6 @@
 function changeLanguage(language) {
-    // Set the language attribute on the <html> or <body> element
     document.documentElement.lang = language;
 
-    // Add or remove classes from the <body> element if necessary
     document.body.className = language;
 
-    // Additional language-specific updates can be performed here
 }
