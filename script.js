2b8771f632cbe873e43db04ab9fd378cdff8bbc0
Daniel <116485787+Wagonwheels40@users.noreply.github.com>
2023-08-10 19:14:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const username = "Wagonwheels40";
-const url = `https://api.github.com/users/${username}/repos?type=owner&sort=stars&direction=desc`;
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const tableBody = document.querySelector("#project-table tbody");
-    data.forEach(item => {
-      if (item.language === "JavaScript") {
-        const row = document.createElement("tr");
-        const nameCell = document.createElement("td");
-        nameCell.textContent = item.name;
-        row.appendChild(nameCell);
-        const descriptionCell = document.createElement("td");
-        descriptionCell.textContent = item.description || "No description provided.";
-        row.appendChild(descriptionCell);
-        const starsCell = document.createElement("td");
-        starsCell.textContent = item.stargazers_count.toLocaleString();
-        row.appendChild(starsCell);
-        const forksCell = document.createElement("td");
-        forksCell.textContent = item.forks_count.toLocaleString();
-        row.appendChild(forksCell);
-        tableBody.appendChild(row);
-      }
-    });
-  })
-  .catch(error => console.error(error));
+const sentence = "I'm Daniel, and I'm a 28 year old front-end developer.";
+const typingEffect = document.getElementById("typing-effect");
+const modal = document.getElementById("resume-modal");
+const openModalButton = document.getElementById("open-modal-btn");
+const dropdownButtons = document.getElementsByClassName("dropdown-btn");
+
+function typeOutSentence(sentence) {
+  let i = 0;
+  const typingInterval = setInterval(() => {
+    if (i < sentence.length) {
+      typingEffect.textContent += sentence.charAt(i);
+      i++;
+    } else {
+      clearInterval(typingInterval);
+    }
+  }, 60);
+}
+
+function openModal() {
+  $('#resume-modal').modal('show'); // Use Bootstrap's modal show method
+  overlay.classList.add("active"); // Add the 'active' class to the overlay
+  document.body.classList.add("modal-open");
+}
+
+function closeModal() {
+  $('#resume-modal').modal('hide'); // Use Bootstrap's modal hide method
+  overlay.classList.remove("active"); // Remove the 'active' class from the overlay
+  document.body.classList.remove("modal-open");
+}
+
+typeOutSentence(sentence);
